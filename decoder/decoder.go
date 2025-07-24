e58db7dff7c9aca0df8cdf4c66025e9bafb19d82
Farseen <itsfarseen@gmail.com>
2025-07-24 14:22:37 +0530

    v9 init

--- decoder/decoder.go
+++ decoder/decoder.go
@@ -3,10 +3,12 @@ package decoder
 import (
 	"fmt"
 	. "submarine/decoder/models"
+	v9 "submarine/decoder/v9"
 	v12 "submarine/decoder/v12"
 	v13 "submarine/decoder/v13"
 	v14 "submarine/decoder/v14"
 	"submarine/scale"
+	scale_v9 "submarine/scale/v9"
 	scale_v12 "submarine/scale/v12"
 	scale_v13 "submarine/scale/v13"
 	scale_v14 "submarine/scale/v14"
@@ -14,6 +16,12 @@ import (
 
 func DecodeMetadata(version uint, r *scale.Reader) (any, error) {
 	switch version {
+	case 9:
+		meta, err := scale_v9.DecodeMetadata(r)
+		if err != nil {
+			return nil, fmt.Errorf("v9: %w", err)
+		}
+		return &meta, nil
 	case 12:
 		meta, err := scale_v12.DecodeMetadata(r)
 		if err != nil {
@@ -29,7 +37,7 @@ func DecodeMetadata(version uint, r *scale.Reader) (any, error) {
 	case 14:
 		meta, err := scale_v14.DecodeMetadata(r)
 		if err != nil {
-			return nil, fmt.Errorf("v14: %w", err)
+			return nil, err
 		}
 		return &meta, nil
 	default:
@@ -45,6 +53,8 @@ func DecodeExtrinsic(metadata any, extrinsicBytes []byte) (*DecodedExtrinsic, er
 		return v13.DecodeExtrinsic(meta, extrinsicBytes)
 	case *scale_v12.Metadata:
 		return v12.DecodeExtrinsic(meta, extrinsicBytes)
+	case *scale_v9.Metadata:
+		return v9.DecodeExtrinsic(meta, extrinsicBytes)
 	default:
 		return nil, fmt.Errorf("unsupported metadata type for extrinsic decoding: %T", metadata)
 	}
@@ -58,6 +68,8 @@ func DecodeEvents(metadata any, eventBytes []byte) ([]EventRecord, error) {
 		return v13.DecodeEvents(meta, eventBytes)
 	case *scale_v12.Metadata:
 		return v12.DecodeEvents(meta, eventBytes)
+	case *scale_v9.Metadata:
+		return v9.DecodeEvents(meta, eventBytes)
 	default:
 		return nil, fmt.Errorf("unsupported metadata type for event decoding: %T", metadata)
 	}
